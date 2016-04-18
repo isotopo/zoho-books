9948f6cb4b07c8d088747ae5ed5ccb114b08d2b7
serchserch <serch.chk@gmail.com>
2016-04-18 17:28:14 -0500

    Add lib test

--- test/config.js
+++ test/config.js
@@ -2,7 +2,8 @@
 
 module.exports = require('rc')('zohobooks', {
   authtoken: process.env.ZOHO_BOOKS_TOKEN || 'YouCanPutYourZohoInvoiceTokenHere',
-  host: 'https://www.zoho.com/books/api/v3',
+  host: 'https://books.zoho.com/api/v3',
+  organization: '000000000',
   email: 'example@example.com',
   password: 'my-password'
 })
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const assert = require('assert')
+const faker = require('faker')
 const ZohoBooks = require('../')
 const config = require('./config')
 
@@ -10,19 +11,79 @@ describe('Zohobooks unit test', function () {
   it('Should be an instance of ZohoBooks', function () {
     assert(zohobooks instanceof ZohoBooks)
   })
-  it('zohobooks intances options should be equal to config', function () {
+  it.skip('zohobooks intances options should be equal to config', function () {
     assert.deepEqual(zohobooks.options, config)
   })
 })
 
 describe('Zohobooks service test', function () {
-  it('Should get a token', function (done) {
+  it.skip('Should get a token', function (done) {
     zohobooks.createToken(config.email, config.password)
-      .then(function(val){
+      .then(function (val) {
         assert(val.authtoken)
         done()
       })
-      .catch(function(err){
+      .catch(function (err) {
+        done(err)
+      })
+  })
+
+  it('Should save an organization', function (done) {
+    zohobooks.api('/organizations', 'POST', {
+      name: faker.lorem.words(2),
+      currency_code: 'MXN',
+      time_zone: 'America/Mexico_City',
+      address: {
+        country: 'Mexico'
+      }
+    })
+      .then(function (val) {
+        assert(val)
+        assert.equal(val.code, 0)
+        done()
+      })
+      .catch(function (err) {
+        done(err)
+      })
+  })
+
+  it('Should get all contacts', function (done) {
+    zohobooks.api('/contacts', 'GET')
+      .then(function (val) {
+        assert(val)
+        assert.equal(val.code, 0)
+        assert(Array.isArray(val.contacts))
+        done()
+      })
+      .catch(function (err) {
+        done(err)
+      })
+  })
+
+  it('Should create a contact', function (done) {
+    zohobooks.api('/contacts', 'POST', {
+      contact_name: faker.name.firstName()
+    })
+      .then(function (val) {
+        assert(val)
+        assert.equal(val.code, 0)
+        assert.equal(typeof val.contact, 'object')
+        done()
+      })
+      .catch(function (err) {
+        done(err)
+      })
+  })
+
+  it.skip('Should get all contacts', function (done) {
+    zohobooks.api('/contacts', 'GET')
+      .then(function (val) {
+        assert(val)
+        assert.equal(val.code, 0)
+        assert(Array.isArray(val.contacts))
+        done()
+      })
+      .catch(function (err) {
         done(err)
       })
   })
