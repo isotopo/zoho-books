0252a1f1df4d75f12f10e0e956996a748f920a5d
serchserch <serch.chk@gmail.com>
2016-04-19 13:46:54 -0500

    Fix env var org

--- test/config.js
+++ test/config.js
@@ -3,7 +3,7 @@
 module.exports = require('rc')('zohobooks', {
   authtoken: process.env.ZOHO_BOOKS_TOKEN || 'YouCanPutYourZohoInvoiceTokenHere',
   host: process.env.ZOHO_BOOKS_HOST || 'https://books.zoho.com/api/v3',
-  organization: process.env.ZOHO_BOOKS_HOST || '000000000',
+  organization: process.env.ZOHO_BOOKS_ORG || '000000000',
   email: process.env.ZOHO_BOOKS_EMAIL || 'example@example.com',
   password: process.env.ZOHO_BOOKS_PASSWORD || 'my-password'
 })
